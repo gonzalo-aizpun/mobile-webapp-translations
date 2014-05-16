ecd4de5a2839280915d3534431e4707f3fa9e016
Nicolas Molina <nicolas.molina@olx.com>
2014-05-16 12:11:34 -0300

    Mejora de tiempos de busqueda

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var ejs = require('ejs');
 var fs = require('fs');
+var csv = require('csv');
+var asynquence = require('asynquence');
 var app = express();
 
 
@@ -23,6 +25,19 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
 
+// Constants
+// ===========================
+
+var languages = [
+    'en-US', 'es-AR', 'es-ES', 'es-EC', 'es-SV', 'fr-FR', 'ht-HT', 'ja-JP', 'ml-IN', 'no-NO', 'ro-RO', 'sr-RS',
+    'te-IN', 'uk-UA', 'ar-AE', 'ca-ES', 'en-IN', 'bs-BA', 'es-VZ', 'gu-IN', 'hu-HU', 'kn-IN', 'mr-IN', 'pa-PK',
+    'ru-RU', 'sv-SE', 'th-TH', 'ur-PK', 'ar-EG', 'cs-CZ', 'af-ZA', 'es-GT', 'et-EE', 'he-IL', 'id-ID', 'ko-KR',
+    'ms-MY', 'pl-PL', 'si-LK', 'sw-TZ', 'th-TW', 'vi-VN', 'bg-BG', 'da-DK', 'el-GR', 'es-MX', 'fi-FI', 'hi-IN',
+    'is-IS', 'lt-LT', 'nl-BG', 'pt-BR', 'sk-SK', 'tl-PH', 'zh-CN', 'bn-BD', 'de-DE', 'es-CO', 'es-PE', 'fr-CA',
+    'hr-HR', 'it-IT', 'lv-LV', 'nl-NL', 'pt-PT', 'sl-SI', 'ta-IN', 'tr-TR', 'zh-TW'
+];
+
+
 // Routes
 // ===========================
 
@@ -31,7 +46,8 @@ app.set('view engine', 'html');
 
     function handler(req, res) {
         res.render('index', {
-            title: 'Keyword search'
+            title: 'Keyword search',
+            languages: languages
         });
     }
 })();
@@ -39,106 +55,118 @@ app.set('view engine', 'html');
 (function search() {
     app.post('/search', handler);
 
-    function find(keyword, filename) {
-        var matchs = [];
-        var translation;
-        filename = __dirname + '/translations/smaug-translations-0.1.161/' + filename + '.csv';
-        var translations = fs.readFileSync(filename);
-        translations = translations.toString().split('\n');
-        translations.forEach(function eachLines(line, index) {
-            if (!line) {
-                return;
-            }
-
-            translation = line.split('","');
-            if (~translation[1].indexOf(keyword) || ~translation[2].indexOf(keyword)) {
-                matchs.push({
-                    translation: {
-                        key: translation[1],
-                        value: translation[2].substr(0, translation[2].length - 2)
-                    }, 
-                    index: (index + 1)
-                });
-            }
-        });
-        return matchs;
-    }
-
     function handler(req, res) {
-        var file = 'en-US';
-        var matchs = find(req.body.keyword, file);
-        res.render('result', {
-            title: 'Result - Keyword search',
-            keyword: req.body.keyword,
-            matchsLength: matchs.length,
-            matchs: matchs,
-            file: file
-        });
+        var matchs = [];
+        var language = req.body.language;
+        var keyword = req.body.keyword;
+
+        function fail(err) {
+            if (err) {
+                console.log(err);
+            }
+            success(' - ERROR');
+        }
+
+        function findMatchs(done) {
+            var filename = [__dirname, '/translations/smaug-translations-0.1.161/', language, '.csv'].join('');
+
+            csv().from(filename).on('record', function onData(record, index) {
+                record = record.slice(1);
+                if (~record[0].indexOf(keyword) || ~record[1].indexOf(keyword)) {
+                    matchs.push({
+                        translation: {
+                            key: record[0],
+                            value: record[1]
+                        }, 
+                        index: (index + 1)
+                    });
+                }
+            }).on('end', done);
+        }
+
+        function success(message) {
+            var title = 'Result - Keyword search';
+
+            if (typeof message === 'string') {
+                title += message;
+            }
+            res.render('result', {
+                title: title,
+                keyword: keyword,
+                matchsLength: matchs.length,
+                matchs: matchs,
+                file: language,
+                languages: languages
+            });
+        }
+
+        asynquence().or(fail)
+            .then(findMatchs)
+            .val(success);
     }
 })();
 
 (function key() {
     app.get('/search/key/:key', handler);
 
-    var files = [
-        'en-US', 'es-ES', 'el-GR', 'es-EC', 'es-SV', 'fr-FR', 'ht-HT', 'ja-JP', 'ml-IN', 'no-NO', 'ro-RO', 'sr-RS',
-        'te-IN', 'uk-UA', 'ar-AE', 'ca-ES', 'en-IN', 'bs-BA', 'es-VZ', 'gu-IN', 'hu-HU', 'kn-IN', 'mr-IN', 'pa-PK',
-        'ru-RU', 'sv-SE', 'th-TH', 'ur-PK', 'ar-EG', 'cs-CZ', 'af-ZA', 'es-GT', 'et-EE', 'he-IL', 'id-ID', 'ko-KR',
-        'ms-MY', 'pl-PL', 'si-LK', 'sw-TZ', 'th-TW', 'vi-VN', 'bg-BG', 'da-DK', 'es-AR', 'es-MX', 'fi-FI', 'hi-IN',
-        'is-IS', 'lt-LT', 'nl-BG', 'pt-BR', 'sk-SK', 'tl-PH', 'zh-CN', 'bn-BD', 'de-DE', 'es-CO', 'es-PE', 'fr-CA',
-        'hr-HR', 'it-IT', 'lv-LV', 'nl-NL', 'pt-PT', 'sl-SI', 'ta-IN', 'tr-TR', 'zh-TW'
-    ];
-
-    function find(key, filename) {
-        var matchs = [];
-        var translation;
-        var file = __dirname + '/translations/smaug-translations-0.1.161/' + filename + '.csv';
-        var translations = fs.readFileSync(file);
-        translations = translations.toString().split('\n');
-        translations.forEach(function eachLines(line, index) {
-            if (!line) {
-                return;
-            }
-
-            translation = line.split('","');
-            if (translation[1] === key) {
-                matchs.push({
-                    translation: translation[2].substr(0, translation[2].length - 2),
-                    file: filename
-                });
-            }
-        });
-        if (matchs.length) {
-            return matchs[0];
-        }
-    }
-
-    function findKey(key) {
-        var matchs = [];
-        var match;
-        files.forEach(function findKeyInFile(file) {
-            match = find(key, file);
-            if (match) {
-                matchs.push(find(key, file));
-                return;
-            }
-            matchs.push({
-                file: file
-            });
-        });
-        return matchs;
-    }
-
     function handler(req, res) {
+        var matchs = [];
         var key = req.param('key', '');
-        var matchs = findKey(key);
-        res.render('key', {
-            title: 'Key - Keyword search',
-            key: key,
-            matchsLength: matchs.length,
-            matchs: matchs
-        });
+        var keyword = req.param('keyword', '');
+        var language = req.param('language', '');
+
+        function fail(err) {
+            if (err) {
+                console.log(err);
+            }
+            success(' - ERROR');
+        }
+
+        function findMatchs(done) {
+            var promise = asynquence().or(done.fail);
+
+            languages.forEach(function findKeyInFile(file) {
+                promise.then(function findMatch(next) {
+                    var filename = [__dirname, '/translations/smaug-translations-0.1.161/', file, '.csv'].join('');
+
+                    csv().from(filename).on('record', function onData(record, index) {
+                        record = record.slice(1);
+                        if (record[0] === key) {
+                            matchs.push({
+                                translation: record[1],
+                                file: file
+                            });
+                            this.pause();
+                            this.end();
+                        }
+                    }).on('end', next);
+                });
+            });
+            promise.val(done);
+        }
+
+        function success(message) {
+            var title = 'Key - Keyword search';
+
+            if (typeof message === 'string') {
+                title += message;
+            }
+            res.render('key', {
+                title: title,
+                key: key,
+                keyword: keyword,
+                matchsLength: matchs.length,
+                matchs: matchs,
+                file: language,
+                languages: languages
+            });
+        }
+
+        asynquence().or(fail)
+            .then(findMatchs)
+            .val(success);
     }
+
 })();
 
 app.listen(4000);
